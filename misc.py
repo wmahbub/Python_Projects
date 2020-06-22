04cedbbe4ccab03e731be3c502e4fd2e96c547e0
wmahbub <62258782+wmahbub@users.noreply.github.com>
2020-06-22 00:55:07 -0400

    added new formulas

--- misc.py
+++ misc.py
@@ -1,3 +1,7 @@
+# Libraries
+
+import math
+
 # Formulas
 
 def square_num(a):
@@ -7,8 +11,31 @@ def square_num(a):
     
     return answer
 
+def sum_of_squares(a,b):
+    """ Sums squares of numbers between (a) and (b). """
+    
+    total=0
+    
+    for i in range(a,b+1):
+        ans= square_num(i)
+        total += ans
+    
+    return total
 
-def is_square_num(a):
+
+def sum_of_only_squares(a,b):
+    """ Sums only square numbers between (a) and (b)."""
+    
+    total = 0
+    
+    for i in range(a, b+1):
+        if is_square_num2(i) == True:
+            total += i
+    
+    return total 
+
+
+def is_square_num1(a):
     """ Checks if the number is a square number or not. """
     lst = []
     
@@ -17,6 +44,18 @@ def is_square_num(a):
         list.append(i)
     
     if a in lst:
+        return True
+    else:
+        return False
+
+
+def is_square_num2(a):
+    """ Checks if the number is a square number or not. """
+    
+    ans = math.sqrt(a)
+    f, i = math.modf(ans)
+    
+    if f == 0 :
         return True
     else:
         return False
@@ -44,5 +83,48 @@ def is_triangle_num(a):
     else:
         return False 
 
+
+def circle_slices(a):
+    """ Calculates the maximum number of pieces that can be made from (a) slices. """
+    
+    assert a == int
+    
+    if a >= 0:
+        ans = ( (a**2) + a + 2 )/2
         
+        return ans
+    
+    else:
+        return "Slices must be greater than or equal to 0"
+    
+def list_slices(a,b):
+    """ Lists the number of pieces that can be made from (a) to (b) slices. """
+    
+    slices = []
+    
+    for i in range(a, b+1):
+        ans = circle_slices(i)
+        slices.append(ans)
+    
+    return slices 
+    
+
+def check_pieces(a):
+    """ jf """
+    
+    pieces= []
+    
+    for i in range(a+1):
+        ans= circle_slices(i)
+        pieces.append(ans)
+    
+    if a in pieces:
+        return True
+    
+    else:
+        return False
+
+def num_pieces(a):
+    """ Returns the number of slices you would need to generate (a) pieces. """
+    
     
